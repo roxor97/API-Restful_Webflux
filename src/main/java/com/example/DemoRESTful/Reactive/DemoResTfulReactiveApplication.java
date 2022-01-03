944f3ad75ca7dfcca8a30c440a26de352ff35452
Roxor <junyperez97@gmail.com>
2022-01-03 12:47:41 -0500

    se agrego la configuracion

---
+++ src/main/java/com/example/DemoRESTful/Reactive/DemoResTfulReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoRESTful.Reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoResTfulReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoResTfulReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoRESTful/Reactive/DemoResTfulReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoRESTful.Reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResTfulReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
