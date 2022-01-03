7f50b88be081fb07126c58ee333a6566d2e9f402
Roxor <junyperez97@gmail.com>
2022-01-03 13:04:04 -0500

    se agrega la clase dato

---
+++ src/main/java/com/example/DemoRESTful/Reactive/Collections/Dato.java
@@ -0,0 +1,28 @@
+package com.example.DemoRESTful.Reactive.Collections;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Dato {
+    @Id
+    private String id;
+    private String informacion;
+    //Getters and Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getInformacion() {
+        return informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+}
